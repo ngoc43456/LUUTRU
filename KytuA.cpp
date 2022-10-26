8f625d1e9a238dfad144f76588807b2441f996c1
Bichngoc <ngoc43456@gmail.com>
2022-10-26 09:11:12 +0700

    kytuA

---
+++ KytuA.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch='A';
+	switch(ch)
+	{
+		case 'a': printf("Ky tu a");break;
+		case 'B': printf("Ky tu B");break;
+		case 'b': printf("Ky tu b");break;
+		case 'A': printf("Ky tu A");break;
+		case 'C': printf("Ky tu C");break;
+	}
+	return 0;
+}
